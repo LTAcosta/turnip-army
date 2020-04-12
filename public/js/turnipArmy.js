09140f0709ad9943931dff53c8cd16e9caa26d38
Lennon Acosta <lennon@tive.co>
2020-04-12 00:24:22 -0400

    Initial authentication support.

--- public/js/turnipArmy.js
+++ public/js/turnipArmy.js
@@ -17,4 +17,27 @@ document.addEventListener('DOMContentLoaded', function() {
       console.error(e);
       document.getElementById('firebase-load').innerHTML = 'Error loading the Firebase SDK, check the console.';
     }
-  });
+});
+
+$("#login").on("click", function () {
+    var provider = new firebase.auth.GoogleAuthProvider();
+
+    firebase.auth().signInWithPopup(provider).then(function(result) {
+        // This gives you a Google Access Token. You can use it to access the Google API.
+        var token = result.credential.accessToken;
+        // The signed-in user info.
+        var user = result.user;
+        // ...
+        $('#login-status').text('Logged in as: ' + user.displayName);
+      }).catch(function(error) {
+        // Handle Errors here.
+        console.log("Error authenticating user:", error);
+        var errorCode = error.code;
+        var errorMessage = error.message;
+        // The email of the user's account used.
+        var email = error.email;
+        // The firebase.auth.AuthCredential type that was used.
+        var credential = error.credential;
+        // ...
+      });
+});
