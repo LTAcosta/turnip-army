a3e2fe1ead86b65c0caa415ef03192a876f5cd86
Lennon Acosta <lennon@tive.co>
2020-04-12 14:49:39 -0400

    Persist login status.

--- public/js/turnipArmy.js
+++ public/js/turnipArmy.js
@@ -1,43 +1,42 @@
 document.addEventListener('DOMContentLoaded', function() {
-    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-    // // The Firebase SDK is initialized and available here!
-    //
-    // firebase.auth().onAuthStateChanged(user => { });
-    // firebase.database().ref('/path/to/ref').on('value', snapshot => { });
-    // firebase.messaging().requestPermission().then(() => { });
-    // firebase.storage().ref('/path/to/ref').getDownloadURL().then(() => { });
-    //
-    // // 🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥🔥
-
-    try {
-      let app = firebase.app();
-      let features = ['auth', 'database', 'messaging', 'storage'].filter(feature => typeof app[feature] === 'function');
-      document.getElementById('firebase-load').innerHTML = `Firebase SDK loaded with ${features.join(', ')}`;
-    } catch (e) {
-      console.error(e);
-      document.getElementById('firebase-load').innerHTML = 'Error loading the Firebase SDK, check the console.';
+    function database() {
+        return firebase.database().ref();
     }
-});
-
-$("#login").on("click", function () {
-    var provider = new firebase.auth.GoogleAuthProvider();
-
-    firebase.auth().signInWithPopup(provider).then(function(result) {
-        // This gives you a Google Access Token. You can use it to access the Google API.
-        var token = result.credential.accessToken;
-        // The signed-in user info.
-        var user = result.user;
-        // ...
-        $('#login-status').text('Logged in as: ' + user.displayName);
-      }).catch(function(error) {
-        // Handle Errors here.
-        console.log("Error authenticating user:", error);
-        var errorCode = error.code;
-        var errorMessage = error.message;
-        // The email of the user's account used.
-        var email = error.email;
-        // The firebase.auth.AuthCredential type that was used.
-        var credential = error.credential;
-        // ...
-      });
+    
+    firebase.auth().onAuthStateChanged(function(user) {
+        updateLoginStatus();
+    });
+    
+    function updateLoginStatus() {
+        var user = firebase.auth().currentUser;
+    
+        if (user) {
+            var userId = user.uid;
+            var username = user.displayName;
+          
+            //chat.setUser(userId, username);
+            $('#login-status').text('Logged in as: ' + username);
+        } else {
+            $('#login-status').text('Logged out.');
+        }
+    }
+    
+    $("#login").on("click", function () {
+        login();    
+    });
+    
+    function login() {
+        var provider = new firebase.auth.GoogleAuthProvider();
+    
+        firebase.auth().signInWithPopup(provider).catch(function(error) {
+            // Handle Errors here.
+            console.log("Error authenticating user:", error);
+        });
+    }
+    
+    function logout() {
+        firebase.auth().signOut().catch(function(error) {
+            console.log("Error signing user out:", error);
+        });
+    }
 });
